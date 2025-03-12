840f9569ce3b8a8f7e44f50b91b23077c000e18c
jing682 <k00290682@student.tus.ie>
2025-03-12 11:28:46 -0700

    Initial Commit
    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assessment3_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======== Guess The Number ========");
+            Console.Write("Guess a number between 1 and 1000 : ");
+            int number = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
