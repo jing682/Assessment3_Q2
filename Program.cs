c48fbbf2b793551da1df969d16e521e16f37ce03
jing682 <k00290682@student.tus.ie>
2025-03-12 12:01:57 -0700

    Update Program.cs
    Check for number of tries

--- Program.cs
+++ Program.cs
@@ -11,26 +11,38 @@ namespace Assessment3_Q2
 
             Console.WriteLine("======== Guess The Number ========");
             Console.Write("Guess a number between 1 and 1000 : ");
-
             while (numTries > 0) {
                 int number = int.Parse(Console.ReadLine());
-
-                if (number < randomNumber)
-                {
-                    Console.WriteLine("Too Low, Try Again");
-                }
-                else if (number > randomNumber)
-                {
-                    Console.WriteLine("Too High, Try Again");
-                }
-                else 
-                {
-                    Console.WriteLine("You guessed corretly!");
-                }
+                numTries--;
+                Console.WriteLine("Number of tries left : " + numTries);
                 
-                
+                if (number < randomNumber && numTries != 0)
+                {
+                    Console.Write("Too Low, Try Again : ");
+                }
+                else if (number > randomNumber && numTries != 0)
+                {
+                    Console.Write("Too High, Try Again : ");
+                }
+                else if (number == randomNumber)
+                {
+                    if (numTries > 0)
+                    {
+                        Console.WriteLine("Either you know the secret or you got lucky!");
+                    }
+                    else if (numTries == 0)
+                    {
+                        Console.WriteLine("You know the secret!");
+                        break;
+                    }
+                }
 
+                if (numTries == 0) 
+                {
+                    Console.WriteLine("You should be able to do better!");
+                }
             }
         }
     }
 }
+
