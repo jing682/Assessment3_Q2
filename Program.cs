0b546f2500b887991f7e33655099850954c5dea6
jing682 <k00290682@student.tus.ie>
2025-03-12 11:32:45 -0700

    Update Program.cs
    Added random number generator

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
-﻿namespace Assessment3_Q2
+namespace Assessment3_Q2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int numTries = 10;
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 1001);
+
             Console.WriteLine("======== Guess The Number ========");
             Console.Write("Guess a number between 1 and 1000 : ");
-            int number = int.Parse(Console.ReadLine());
 
+            while (numTries > 0) {
+                int number = int.Parse(Console.ReadLine());
+                
+                
 
+            }
         }
     }
 }
